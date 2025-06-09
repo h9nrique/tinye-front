53261ec3d08b5c95d0fd7b6394254946e28a3b83
Bruno Henrique Pereira <b.hpereira97@gmail.com>
2025-06-09 09:18:14 -0300

    Adicionado skeleton no fallback dos links

--- src/app/(default)/profile/page.tsx
+++ src/app/(default)/profile/page.tsx
@@ -1,12 +1,13 @@
 import React, { Suspense } from "react";
 import Container from "@/components/ui/Container";
 import LinksList from "@/components/profile/LinksList";
+import LinksListsSkeleton from "@/components/profile/LinksListsSkeleton";
 
 export default function Profile() {
   return (
     <Container>
       <h1 className="text-2xl font-bold mb-4">Meus links</h1>
-      <Suspense fallback={<div>Carregando links...</div>}>
+      <Suspense fallback={<LinksListsSkeleton />}>
         <LinksList />
       </Suspense>
     </Container>
