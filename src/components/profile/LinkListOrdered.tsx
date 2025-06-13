09e6383b066cd42c91aa0b88bff1a1690bcd170c
Bruno Henrique Pereira <b.hpereira97@gmail.com>
2025-06-12 21:57:42 -0300

    Ajuste: Agora é mostrado a quantidade de links do usuário e o limite máximo

--- src/components/profile/LinkListOrdered.tsx
+++ src/components/profile/LinkListOrdered.tsx
@@ -4,6 +4,12 @@ import React, { useState } from "react";
 import { LinkType } from "@/types/links/LinkType";
 import LinkCardProfile from "./LinkCardProfile";
 import CreateLink from "./CreateLink";
+import { IoMdInformationCircle } from "react-icons/io";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function LinkListOrdered({
   orderedLinks,
@@ -13,6 +19,8 @@ export default function LinkListOrdered({
   const [orderedLinksList, setOrderedLinksList] =
     useState<LinkType[]>(orderedLinks);
 
+  const reachedMaximunQuantity = orderedLinksList.length >= 10;
+
   return (
     <div>
       <section>
@@ -20,7 +28,32 @@ export default function LinkListOrdered({
         <CreateLink setOrderedLinksList={setOrderedLinksList} />
       </section>
       <section>
-        <h1 className="text-2xl font-bold mb-2 ml-1">Meus links</h1>
+        <div className="flex justify-between items-center mr-2">
+          <h2 className="text-2xl font-bold mb-2 ml-1">Meus links</h2>
+          <div className="flex items-center">
+            <p
+              className={`${
+                reachedMaximunQuantity ? "text-red-500" : "text-gray-500"
+              } font-medium`}
+            >
+              {orderedLinksList.length}/10 links
+            </p>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <IoMdInformationCircle
+                  className={`ml-1 text-gray-500 ${
+                    reachedMaximunQuantity ? "text-red-500" : "text-gray-500"
+                  }`}
+                  size={16}
+                />
+              </TooltipTrigger>
+              <TooltipContent align="end" side="bottom" className="bg-primary">
+                <p>No plano básico, o limite é de 10 links</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </div>
+
         {orderedLinksList
           .sort((a, b) => {
             const dateA = new Date(a.createdAt).getTime();
