4d348a109cbf9ceca064daca3c8daf8ff0cda7a3
Bruno Henrique Pereira <b.hpereira97@gmail.com>
2025-06-17 08:43:03 -0300

    fix. Bug - Alguns clique não funcionável nos filtros

--- src/components/profile/FilterRadioItem.tsx
+++ src/components/profile/FilterRadioItem.tsx
@@ -15,20 +15,16 @@ export default function FilterRadioItem({
   label,
 }: FilterRadioItemProps) {
   return (
-    <div
-      className={`flex items-center border px-2 py-1 rounded-full duration-300 transition-colors w-full justify-center  ${
+    <Label
+      htmlFor={value}
+      className={`flex items-center border px-2 py-1 rounded-full duration-300 transition-colors w-full justify-center cursor-pointer ${
         filter === value
           ? "border-blue-800 text-blue-800"
           : "border-gray-400 text-gray-400 hover:border-gray-800 hover:text-gray-800"
       }`}
     >
       <RadioGroupItem value={value} id={value} className="hidden" />
-      <Label
-        htmlFor={value}
-        className="text-xs mb-0 cursor-pointer text-nowrap"
-      >
-        {label}
-      </Label>
-    </div>
+      <p className="text-xs mb-0 text-nowrap">{label}</p>
+    </Label>
   );
 }
